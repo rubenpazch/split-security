adce14535ded8c670495656399ddcbbe04d05514
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2023-07-03 23:54:02 -0500

    update smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,14 +113,14 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.ipage.com',
+    address: 'smtp.gmail.com',
     # domain: Rails.application.credentials.zoho[:domain],
-    port: 465,
-    domain: 'ipage.com',
-    user_name: ENV["IPAGE_USERNAME"],
-    password: ENV["IPAGE_PASSWORD"],
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',
-    ssl: true,
+    #ssl: true,
     # tls: true,
     enable_starttls_auto: true
   }
